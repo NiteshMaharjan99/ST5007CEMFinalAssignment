f0bb1dbeaaa7e66ac77d46ab5c9079ee154d0685
NiteshMaharjan99 <niteshmaharjan100@gmail.com>
2022-07-25 20:33:25 +0545

    test case added for url

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.test import Client, TestCase, SimpleTestCase
+from django.urls import reverse,resolve
+from account.views import *
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class TestUrls(SimpleTestCase):
+    # passed test
+    def test_index_url(self):
+        url=reverse('userLogin')
+        self.assertEquals(resolve(url).func,userLogin)
+    # passed test
+    def test_userRegister_url(self):
+        url=reverse('userRegister')
+        self.assertEquals(resolve(url).func,userRegister)
+    
+    # failed test
+    def test_userRegister_url(self):
+        url=reverse('userRegister')
+        self.assertEquals(resolve(url).func,userLogin)
--- ecom/tests.py
+++ ecom/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from django.test import Client, TestCase, SimpleTestCase
+from django.urls import reverse,resolve
+from ecom.views import *
+from django.contrib.auth.models import User
+from .models import *
 
 # Create your tests here.
+# class TestUrls(SimpleTestCase):
+#     def text_index_url(self):
+#         url=reverse('store')
+#         self.assertEquals(resolve(url).func,store)
+
+    # def test_cart_url(self):
+    #     cartItems = Order.()
+    #     url=reverse('cart')
+
+    #     self.assertEquals(resolve(url).func,edit)
+
+# class TestViews(TestCase):
+#     def test_index(self):
+#         user=User.objects.create(username='nitesh')
+#         user.set_password('maharjan100')
+#         user.save()
+#         client = Client()
+#         logged_in=client.login(username='nitesh',password="maharjan100")
+
+
+#         # login_required not needed
+#         response=client.get(reverse('store'))
+#         print(response) # status_code == 200
+#         self.assertEquals(response.status_code,200)
+#         self.assertTemplateUsed(response,"store")
+
+# class TestUrls(SimpleTestCase):
+#     def text_store_url(self):
+#         url=reverse('')
+#         self.assertEquals(resolve(url).func,store)
